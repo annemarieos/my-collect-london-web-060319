3682ce97128f10326d64e55c8f443fcd4d92cb57
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-28 10:24:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
-collect = []
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
-def my_collect(array)
-  i = 0
-  while i < array.length
-    collect << yield(array[i])
-    i + 1
-  end
-  collect
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
 end
